7592d6efe268f8bc1dd9f24f95b697432bd75816
ManzoorWayar <softwareengineerwayar@gmail.com>
2023-01-15 14:05:28 +0430

    remove authrition

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,14 +1,15 @@
 const authChecker = (subject, roll) => {
 	return (req, res, next) => {
-		if (
-			req.user.role.subject === subject &&
-			req.user.role.ability.includes(roll)
-		) {
-			next()
-		} else {
-			res.status(401)
-			throw new Error("unauthorized access")
-		}
+		// if (
+		// 	req.user.role.subject === subject &&
+		// 	req.user.role.ability.includes(roll)
+		// ) {
+		// 	next()
+		// } else {
+		// 	res.status(401)
+		// 	throw new Error("unauthorized access")
+		// }
+		next()
 	}
 }
 export default authChecker
