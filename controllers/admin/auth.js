eabde2b540ef473266aee32c515c2dbc940a8ba3
ManzoorWayar <softwareengineerwayar@gmail.com>
2023-01-15 13:30:40 +0430

    cookir replace

--- controllers/admin/auth.js
+++ controllers/admin/auth.js
@@ -45,7 +45,7 @@ const login = asyncHandler(async (req, res, next) => {
   const options = {
     httpOnly: true, //accessible only by web server 
     secure: process.env.NODE_ENV === "production", //https 
-    SameSite: 'None', //cross-site cookie  
+    SameSite: 'Lux', //cross-site cookie  
     maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
 
 
