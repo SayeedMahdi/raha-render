ee168ec493111398118ad294cf14b77410be7933
ManzoorWayar <softwareengineerwayar@gmail.com>
2023-01-15 13:36:29 +0430

    cookie has been replace

--- controllers/admin/auth.js
+++ controllers/admin/auth.js
@@ -48,8 +48,6 @@ const login = asyncHandler(async (req, res, next) => {
     SameSite: 'Lux', //cross-site cookie  
     maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
 
-
-
     // httpOnly: true, //accessible only by web server 
     // secure: true, //https
     // sameSite: 'None', //cross-site cookie 
